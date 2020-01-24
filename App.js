0e15e7590daed22e6175d3bca489b56b11823b60
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2020-01-24 16:19:38 -0300

    coincap API

--- App.js
+++ App.js
@@ -1,8 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button} from 'react-native';
+
+const top100Coins = [];
 
 export default function App() {
   const [outputText, setOutputText] = useState('No data');
+
+// Fetch Top 100 coins from CoinCap
+useEffect(() => {
+  fetch("https://api.coincap.io/v2/assets")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        const coins = result.data;
+        coins.forEach(e => {
+            let newObj = { id: e.id, name: e.name, symbol: e.symbol }
+            top100Coins.push(newObj)
+        });
+
+        let updatedCoins = [...top100Coins] // copy array to set state in an immutable fashion
+      },
+
+      (error) => {
+      }
+    )
+
+});
+
   return (
     <View style={styles.container}>
       <Text>{outputText}</Text>
