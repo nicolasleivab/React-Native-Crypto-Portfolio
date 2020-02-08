303e512fd95bca6cef912b26aeadb858de6f190e
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2020-02-08 16:50:28 -0300

    supply data format

--- screens/Coins/Coins.js
+++ screens/Coins/Coins.js
@@ -49,7 +49,6 @@ useEffect(() => {
           }else{
           d.priceUsd = Math.floor(d.priceUsd * 10000) / 10000;
           }
-          d.supply = Math.floor(d.supply * 10000) / 10000;
           d.maxSupply = Math.floor(d.maxSupply * 10000) / 10000;
           d.changePercent24Hr = Math.floor(d.changePercent24Hr * 100) / 100;
           d.vwap24Hr = Math.floor(d.vwap24Hr * 10000) / 10000;
@@ -72,6 +71,14 @@ useEffect(() => {
             d.volumeUsd24Hr = ((d.volumeUsd24Hr) / 1000000).toFixed(2) + 'M';
           } else {
             d.volumeUsd24Hr = ((d.volumeUsd24Hr) / 1000).toFixed(2) + 'K';
+          }
+          d.supply = +d.supply;
+          if (d.supply >= 1000000000) {
+            d.supply = ((d.supply) / 1000000000).toFixed(2) + 'B';
+          } else if (d.supply >= 1000000) {
+            d.supply = ((d.supply) / 1000000).toFixed(2) + 'M';
+          } else {
+            d.supply = ((d.supply) / 1000).toFixed(2) + 'K';
           }
         });
 
