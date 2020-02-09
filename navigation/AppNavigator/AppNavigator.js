73de18a37cd0cd01779ebfc747ffc9f70d1a8acc
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2020-02-09 11:36:13 -0300

    navigation options

--- navigation/AppNavigator/AppNavigator.js
+++ navigation/AppNavigator/AppNavigator.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createAppContainer } from 'react-navigation';
@@ -6,21 +7,71 @@ import Chart from '../../screens/Chart/Chart';
 import Portfolio from '../../screens/Portfolio/Portfolio';
 import NewsFeed from '../../screens/NewsFeed/NewsFeed';
 import Settings from '../../screens/Settings/Settings';
+import Colors from '../../constants/colors';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const ChartNavigator = createStackNavigator({
     Coins: {
         screen: Coins,  
 },
-    Chart: Chart,
+    Chart: {
+        screen: Chart,
+    }
 })
 
 const AppNavigator = createBottomTabNavigator({
     Coins :{
-        screen: ChartNavigator
+        screen: ChartNavigator,
+        navigationOptions: {
+            tabBarIcon: (tabInfo)=> {
+                return <Icon name="logo-bitcoin" size={25} color={tabInfo.tintColor}></Icon>
+            }
+        }
     },
-    Portfolio: Portfolio,
-    NewsFeed: NewsFeed,
-    Settings: Settings
+    Portfolio:{
+        screen:Portfolio,
+        navigationOptions: {
+            tabBarIcon: (tabInfo) => {
+                return <Icon name="ios-pie" size={25} color={tabInfo.tintColor}></Icon>
+            }  
+        }
+    },
+    NewsFeed: {
+        screen: NewsFeed,
+        navigationOptions: {
+            tabBarIcon: (tabInfo) => {
+                return <Icon name="ios-list-box" size={25} color={tabInfo.tintColor}></Icon>
+            }  
+        }
+    },
+    Settings: {
+        screen: Settings,
+        navigationOptions: {
+            tabBarIcon: (tabInfo) => {
+                return <Icon name="ios-settings" size={25} color={tabInfo.tintColor}></Icon>
+            }  
+        }
+    }
+}, {
+    tabBarOptions: {
+        activeBackgroundColor: Colors.primary,
+        inactiveBackgroundColor: Colors.secondary,
+        activeTintColor: Colors.text_primary,
+        
+
+    }
+
 })
+//Hide tab nav for stacked screens
+ChartNavigator.navigationOptions = ({ navigation }) => {
+    let tabBarVisible = true;
+    if (navigation.state.index > 0) {
+        tabBarVisible = false;
+    }
+
+    return {
+        tabBarVisible,
+    };
+};
 
 export default createAppContainer(AppNavigator);
